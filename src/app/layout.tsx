e14e9027bbb872a437db9cb04cb8f0e3a5b6e055
Aaron Zygala <aaronzygala@gmail.com>
2025-02-14 17:34:07 -0600

    testing ios background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+      <meta name="theme-color" content="#000000"/>
+
 
 
       <AOSProvider>
